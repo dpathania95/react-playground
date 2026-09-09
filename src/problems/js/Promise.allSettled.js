f32dcfb36d5bc317ee027ffeff48595419e76371
Divyanshu Pathania <divyanshu.990pathania@gmail.com>
2026-09-09 16:45:46 +0530

    promise.all polyfill

---
+++ src/problems/js/Promise.allSettled.js
@@ -0,0 +1,40 @@
+function promiseAllSettled(iterable) {
+  return new Promise((resolve) => {
+    let result = [];
+		let settledCount = 0;
+    if (!iterable || iterable.length === 0) {
+      resolve(result);
+    }
+    for (let i=0;i<iterable.length;i++) {
+      const promise = iterable[i];
+      if (promise instanceof Promise) {
+        promise.then((value) => {
+          result[i] = {status: 'fulfilled', value};
+        }).catch((err) => {
+          result[i] = {status: 'rejected', reason: err};
+        }).finally(() => {
+					settledCount++;
+          if (settledCount===iterable.length) {
+            resolve(result);
+          }
+        })
+      } else {
+				settledCount++;
+        result[i] = {status: 'fulfilled', value: promise};
+      }
+    }
+    if (settledCount === iterable.length) {
+      resolve(result);
+    }
+  })
+}
+const p0 = new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(2);
+          }, 10);
+        });
+        const p1 = Promise.resolve(3);
+        const p2 = 4;
+promiseAllSettled([p0, p1, p2]).then((data) => {
+	console.log(data);
+})
